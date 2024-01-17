b92ce4dfde66a46bc5d3afb89511f25ff0a24c1c
Vladimir Afonin <vovan010698@mail.ru>
2024-01-17 23:15:09 +0300

    hw2

--- src/channel.py
+++ src/channel.py
@@ -1,14 +1,28 @@
 import os
 from googleapiclient.discovery import build
 import json
+
+api_key = os.getenv('YOUTUBE_API_KEY')
+
+
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = os.getenv('YOUTUBE_API_KEY')
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        api_key: str = os.getenv('YOUTUBE_API_KEY')
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = channel["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        self.subscriber_count = channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = channel["items"][0]["statistics"]["viewCount"]
+        self.dict = {"title": self.title, "description": self.description, "url": self.url,
+                     "subscriber_count": self.subscriber_count,
+                     "video_count": self.video_count, "self.view_count": self.view_count}
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -17,3 +31,19 @@ class Channel:
             part='snippet,statistics'
         ).execute()
         print(json.dumps(response, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        """Возвращающий объект для работы с YouTube API"""
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, filename):
+        """Сохраняет в файл значения атрибутов экземпляра `Channel`"""
+        channel_dict = self.dict
+        with open(filename, 'w', encoding="windows-1251") as file:
+            json.dump(channel_dict, file, ensure_ascii=False)
