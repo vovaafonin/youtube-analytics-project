4903376d58b1c1d76b8dddaa11b76c80740de451
Vladimir Afonin <vovan010698@mail.ru>
2024-02-03 23:07:00 +0300

    hw6

--- src/video.py
+++ src/video.py
@@ -7,14 +7,21 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_response = self.youtube.videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',
-            id=self.video_id).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.video_url = f"https://www.youtube.com/{self.video_id}"
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.video_response = self.youtube.videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=self.video_id).execute()
+            self.video_title = self.video_response['items'][0]['snippet']['title']
+            self.video_url = f"https://www.youtube.com/{self.video_id}"
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except:
+            self.video_title = None
+            self.view_count = None
+            self.like_count = None
+            self.video_url = None
+
 
     def __str__(self):
         return f"{self.video_title}"
